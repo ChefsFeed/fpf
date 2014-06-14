41709ee043479dd0626b32c5f0b2c65496c80622
Lucas Florio <lucasefe@gmail.com>
2014-06-13 19:49:03 -0700

    Handle dead process by restarting them.

--- lib/config.rb
+++ lib/config.rb
@@ -1,3 +1,6 @@
 require 'redis'
 require 'fpf/fetchers'
-FETCHERS = FullPageFetcher::Fetchers.new(8910, 1)
+port_start = ENV.fetch('FPF_PHANTOM_PORT_START', 8910).to_i
+port_count = ENV.fetch('FPF_PHANTOM_PORT_COUNT', 10).to_i
+
+FETCHERS = FullPageFetcher::Fetchers.new(port_start, port_count)
